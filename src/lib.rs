bb617659aba8f97e25593d4470e3afb4298cbaef
Berkus Decker <berkus@madfire.net>
2018-01-25 18:28:18 +0200

    [sq] Check for 64-bit arm correctly

--- src/lib.rs
+++ src/lib.rs
@@ -2,13 +2,11 @@
 #![feature(asm)]
 #![doc(html_root_url = "https://doc.metta.systems/")]
 
-
-#[cfg(not(any(all(target_arch = "arm", target_pointer_width = "32"),
-                  all(target_arch = "x86"), all(target_arch = "x86_64"))))]
+#[cfg(not(any(target_arch = "aarch64", target_arch = "x86_64")))]
 use architecture_not_supported_sorry;
 
 #[cfg(target_arch = "x86_64")]
 include!("arch/x86_64.rs");
 
-#[cfg(all(target_arch = "arm", target_pointer_width = "64"))]
+#[cfg(target_arch = "aarch64")]
 include!("arch/aarch64.rs");
