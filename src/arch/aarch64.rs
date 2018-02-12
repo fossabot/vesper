030f95efe5dc68a60c6fbf19087cf4ba0fa8a2f5
Berkus Decker <berkus@madfire.net>
2018-02-13 01:08:56 +0200

    Add some arm assembly code

--- src/arch/aarch64.rs
+++ src/arch/aarch64.rs
@@ -9,5 +9,58 @@ pub unsafe extern fn karch_start() -> ! {
 
 // Data memory barrier
 pub fn dmb() {
-    unsafe { asm!("dmb sy"); }
+    unsafe { asm!("dmb sy" :::: "volatile"); } // @fixme this is a full barrier
+}
+
+pub fn flushcache(address: usize) {
+    unsafe { asm!("dc ivac, $0" :: "r"(address) :: "volatile"); }
+}
+
+pub fn read_translation_table_base() -> u64 {
+    let mut base: u64 = 0;
+    unsafe { asm!("mrs $0, ttbr0_el1" : "=r"(base) ::: "volatile"); }
+    return base
+}
+
+pub fn read_translation_control() -> u64 {
+    let mut tcr: u64 = 0;
+    unsafe { asm!("mrs $0, tcr_el1" : "=r"(tcr) ::: "volatile"); }
+    return tcr
+}
+
+pub fn read_mair() -> u64 {
+    let mut mair: u64 = 0;
+    unsafe { asm!("mrs $0, mair_el1" : "=r"(mair) ::: "volatile"); }
+    return mair
+}
+
+pub fn write_translation_table_base(base: usize) {
+    unsafe { asm!("msr ttbr0_el1, $0" :: "r"(base) :: "volatile"); }
+}
+
+// Helper function similar to u-boot
+pub fn write_ttbr_tcr_mair(el: u8, base: u64, tcr: u64, attr: u64)
+{
+    unsafe { asm!("dsb sy" :::: "volatile"); }
+    match (el) {
+        1 => {
+            unsafe { asm!("msr ttbr0_el1, $0
+                msr tcr_el1, $1
+                msr mair_el1, $2" :: "r"(base), "r"(tcr), "r"(attr) : "memory" : "volatile"); }
+        },
+        2 => {
+            unsafe { asm!("msr ttbr0_el2, $0
+                msr tcr_el2, $1
+                msr mair_el2, $2" :: "r"(base), "r"(tcr), "r"(attr) : "memory" : "volatile"); }
+        },
+        3 => {
+            unsafe { asm!("msr ttbr0_el3, $0
+                msr tcr_el3, $1
+                msr mair_el3, $2" :: "r"(base), "r"(tcr), "r"(attr) : "memory" : "volatile"); }
+        },
+        _ => loop{},
+    }
+    unsafe { asm!("isb" :::: "volatile"); }
+}
+
 }
