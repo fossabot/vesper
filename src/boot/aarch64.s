bb18408b9841d99021a9a825f2018935c95df02e
Berkus Decker <berkus@madfire.net>
2018-02-23 16:55:55 +0200

    temporary placement of page tables

--- src/boot/aarch64.s
+++ src/boot/aarch64.s
@@ -2,3 +2,19 @@
 .section .entry, "x"
 _start:
     b karch_start
+
+// Crazy initial pagetables here
+// Identity map first Gigabyte minus 16 meg for physical devices bus
+// Identity map physical devices as well (0x3f000000-..)
+/*section .bss
+align 4096
+p4_table:
+    resb 4096
+p3_table:
+    resb 4096
+p2_table:
+    resb 4096
+stack_bottom:
+    resb 64
+stack_top:
+*/
