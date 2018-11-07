3ac0974eb6ce4e624629cbeb3c648c4ad4571ddb
Berkus Decker <berkus+github@metta.systems>
2018-11-07 08:41:22 +0200

    [wip] cleanup number format

--- src/platform/rpi3.rs
+++ src/platform/rpi3.rs
@@ -1,20 +1,20 @@
 // See BCM2835-ARM-Peripherals.pdf
 // See https://www.raspberrypi.org/forums/viewtopic.php?t=186090 for more details.
 
-// Physical memory is 0x00000000 to 0x40000000
+// Physical memory is 0x0000_0000 to 0x4000_0000
 const fn phys2virt(address: u32) -> u32 {
-    address // + 0x80000000;
+    address // + 0x8000_0000;
 }
 
-// RAM bus address is 0xC0000000 to 0xFFFFFFFF
-// Peripheral bus memory is 0x7E000000 to 0x7EFFFFFF
+// RAM bus address is 0xC000_0000 to 0xFFFF_FFFF
+// Peripheral bus memory is 0x7E00_0000 to 0x7EFF_FFFF
 pub fn phys2bus(address: u32) -> u32 {
-    address.wrapping_add(0xC0000000) // L2 cache disabled
+    address.wrapping_add(0xC000_0000) // L2 cache disabled
 }
 
 pub fn bus2phys(address: u32) -> u32 {
-    address.wrapping_sub(0xC0000000) // L2 cache disabled
+    address.wrapping_sub(0xC000_0000) // L2 cache disabled
 }
 
 pub const PERIPHERAL_BASE: u32 = phys2virt(0x3F00_0000); // Base address for all peripherals
-
+// @todo BcmHost::get_peripheral_address()
