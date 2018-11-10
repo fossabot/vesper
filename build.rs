de1be711f9b5e4ffbc889b6fdc384544b306a6d4
Berkus Decker <berkus+github@metta.systems>
2018-11-10 15:39:50 +0200

    Replace single TARGET env var with match on Cargo-set triple values

--- build.rs
+++ build.rs
@@ -2,7 +2,7 @@
 
 extern crate cc;
 
-use std::{env, path::PathBuf};
+use std::env;
 
 fn build_libboot(arch: &str, llvmtriple: &str) {
     env::set_var("TARGET_CC", "clang");
@@ -18,42 +18,40 @@ fn build_libboot(arch: &str, llvmtriple: &str) {
 // for possible target triple format.
 
 fn main() {
-    let target = env::var("TARGET").unwrap();
-    eprintln!("Target {}", target);
-    let target = if target.ends_with(".json") {
-        PathBuf::from(target).file_stem().expect("Cannot convert target path to triple")
-            .to_str().expect("Path must be valid unicode").into()
-    } else {
-        target
-    };
-    eprintln!("Triple {}", target);
+    let target_arch = env::var("CARGO_CFG_TARGET_ARCH").unwrap();
+    let target_os = env::var("CARGO_CFG_TARGET_OS").unwrap();
+    let target_vendor = env::var("CARGO_CFG_TARGET_VENDOR").unwrap();
+//    let target_env = env::var("CARGO_CFG_TARGET_ENV").unwrap();
 
-    // @todo match on (CARGO_CFG_TARGET_ARCH, CARGO_CFG_TARGET_OS, CARGO_CFG_TARGET_FAMILY) instead
-    // It will be `(aarch64, none, metta)` for example.
+    match (target_arch.as_str(), target_os.as_str(), target_vendor.as_str()) {
+        ("i686", "vesper", "metta") => {
+            build_libboot("x86", "i686-unknown-none");
+            println!("cargo:rustc-link-lib=static=x86");
+            println!("cargo:rerun-if-changed=src/boot/x86.s");
+        },
+        ("x86-64", "vesper", "metta") => {
+            build_libboot("x86_64", "x86_64-unknown-none");
+            println!("cargo:rustc-link-lib=static=x86_64");
+            println!("cargo:rerun-if-changed=src/boot/x86_64.s");
+        },
+        ("arm", "vesper", "metta") => {
+            build_libboot("arm", "arm-linux-gnueabihf");
+            println!("cargo:rustc-link-lib=static=arm");
+            println!("cargo:rerun-if-changed=src/boot/arm.s");
+        },
+        ("aarch64", "vesper", "metta") => {
+            build_libboot("aarch64", "aarch64-unknown-none");
+            println!("cargo:rustc-link-lib=static=aarch64");
+            println!("cargo:rerun-if-changed=src/boot/aarch64.s");
+            println!("cargo:rerun-if-changed=linker/aarch64.ld");
 
-    if target == "i686-vesper-metta" {
-        build_libboot("x86", "i686-unknown-linux-gnu");
-        println!("cargo:rustc-link-lib=static=x86");
-        println!("cargo:rerun-if-changed=src/boot/x86.s");
-    } else if target == "x86_64-vesper-metta" {
-        build_libboot("x86_64", "x86_64-unknown-linux-gnu");
-        println!("cargo:rustc-link-lib=static=x86_64");
-        println!("cargo:rerun-if-changed=src/boot/x86_64.s");
-    } else if target == "arm-vesper-metta" {
-        build_libboot("arm", "arm-linux-gnueabihf");
-        println!("cargo:rustc-link-lib=static=arm");
-        println!("cargo:rerun-if-changed=src/boot/arm.s");
-    } else if target == "aarch64-vesper-metta" {
-        build_libboot("aarch64", "aarch64-unknown-linux-gnu");
-        println!("cargo:rustc-link-lib=static=aarch64");
-        println!("cargo:rerun-if-changed=src/boot/aarch64.s");
-        println!("cargo:rerun-if-changed=linker/aarch64.ld");
-
-    // Todo: generate a runner script that would run objcopy from generated kernel.elf to binary
-    // This script will be ran by `cargo run` command defined in .cargo/config
-    // File::create(out_dir.join("complete.sh")).unwrap()
-    // .write_all(b"Script here").unwrap();
-    } else {
-        panic!("TARGET env variable is not set to one of supported values");
+            // Todo: generate a runner script that would run objcopy from generated kernel.elf to binary
+            // This script will be ran by `cargo run` command defined in .cargo/config
+            // File::create(out_dir.join("complete.sh")).unwrap()
+            // .write_all(b"Script here").unwrap();
+        },
+        _ => {
+            panic!("Target is not set to one of supported values");
+        }
     }
 }
