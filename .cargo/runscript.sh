ffd775a484ae3906e1b8480f5c376237ed14a5e3
Berkus Decker <berkus+github@metta.systems>
2018-11-10 17:33:23 +0200

    Revert "Try and use rustc-bundled LLD for linking"
    b1ca9f5540a89b228514395d0941f528c9db2550

--- .cargo/runscript.sh
+++ .cargo/runscript.sh
@@ -2,5 +2,4 @@
 
 aarch64-unknown-linux-musl-objcopy -O binary $1 $1.bin
 
-#.bin
-qemu-system-aarch64 -M raspi3 -d in_asm -serial stdio -kernel $1
+qemu-system-aarch64 -M raspi3 -d in_asm -serial stdio -kernel $1.bin
