ce7cb9956a1829d863e62498a6d01dd83bf08165
Berkus Decker <berkus+github@metta.systems>
2018-11-13 21:12:47 +0200

    [temp] don't display incoming assembly code

--- .cargo/runscript.sh
+++ .cargo/runscript.sh
@@ -2,4 +2,5 @@
 
 aarch64-unknown-linux-musl-objcopy -O binary $1 $1.bin
 
-/usr/local/Cellar/qemu/HEAD-160e5c2-custom/bin/qemu-system-aarch64 -M raspi3 -d in_asm -serial null -serial stdio -kernel $1.bin
+#-d in_asm
+/usr/local/Cellar/qemu/HEAD-160e5c2-custom/bin/qemu-system-aarch64 -M raspi3 -serial null -serial stdio -kernel $1.bin
