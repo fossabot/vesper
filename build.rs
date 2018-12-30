19db4708f092508858a33004cc6cc4734cc29a8d
Berkus Decker <berkus+github@metta.systems>
2018-12-30 06:11:44 +0200

    replace xargo with cargo-xbuild

--- build.rs
+++ build.rs
@@ -2,7 +2,7 @@
 
 extern crate cc;
 
-use std::env;
+use std::{env, path::PathBuf};
 
 fn build_libboot(arch: &str, llvmtriple: &str) {
     env::set_var("TARGET_CC", "clang");
@@ -14,8 +14,20 @@ fn build_libboot(arch: &str, llvmtriple: &str) {
         .compile(arch);
 }
 
+// See https://github.com/rust-lang/rust/blob/653da4fd006c97625247acd7e076d0782cdc149b/src/librustc_target/spec/mod.rs#L1275-L1278
+// for possible target triple format.
+
 fn main() {
     let target = env::var("TARGET").unwrap();
+    eprintln!("Target {}", target);
+    let target = if target.ends_with(".json") {
+        PathBuf::from(target).file_stem().expect("Cannot convert target path to triple")
+            .to_str().expect("Path must be valid unicode").into()
+    } else {
+        target
+    };
+    eprintln!("Triple {}", target);
+
     if target == "i686-vesper-metta" {
         build_libboot("x86", "i686-unknown-linux-gnu");
         println!("cargo:rustc-link-lib=static=x86");
@@ -34,10 +46,10 @@ fn main() {
         println!("cargo:rerun-if-changed=src/boot/aarch64.s");
         println!("cargo:rerun-if-changed=linker/aarch64.ld");
 
-        // Todo: generate a runner script that would run objcopy from generated kernel.elf to binary
-        // This script will be ran by `cargo run` command defined in .cargo/config
-        // File::create(out_dir.join("complete.sh")).unwrap()
-            // .write_all(b"Script here").unwrap();
+    // Todo: generate a runner script that would run objcopy from generated kernel.elf to binary
+    // This script will be ran by `cargo run` command defined in .cargo/config
+    // File::create(out_dir.join("complete.sh")).unwrap()
+    // .write_all(b"Script here").unwrap();
     } else {
         panic!("TARGET env variable is not set to one of supported values");
     }
